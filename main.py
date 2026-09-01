05c975b627aa0b1f9be71fcc0ea70bb60ec0efcd
Chris kaltsas <140308973+CodebindPyth@users.noreply.github.com>
2026-09-01 21:51:45 +0300

    Refactor user registration and banking options

--- main.py
+++ main.py
@@ -1,83 +1,167 @@
-from register import *
-from bank import *
+```python
+
+import register
+import bank
+
+
+blocked = [
+    "union", "select", "insert", "update", "delete",
+    "drop", "alter", "create", "truncate",
+    "--", ";",
+    "'", '"'
+]
 
 status = False
+
 print("Welcome to Mohit Banking Project")
+
 while True:
     try:
-        register = int(input("1. SignUp\n"
-                             "2. SignIn"))
-        if register == 1 or register == 2:
-            if register == 1:
-                SignUp()
-            if register == 2:
-                user = SignIn()
-                status = True
-                break
-        else:
-            print("Please Enter Valid Input From Options")
+        choice = int(input(
+            "\n1. SignUp\n"
+            "2. SignIn\n"
+            "Choose an option: "
+        ))
 
-    except ValueError:
-        print("Invalid Input Try Again with Numbers")
+        if choice == 1:
+            register.SignUp()
 
-account_number = db_query(
-    f"SELECT account_number FROM customers WHERE username = '{user}';")
+        elif choice == 2:
+            user = register.SignIn()
 
-while status:
-    print(f"Welcome {user.capitalize()} Choose Your Banking Service\n")
-    try:
-        facility = int(input("1. Balance Enquiry\n"
-                             "2. Cash Deposit\n"
-                             "3. Cash Withdraw\n"
-                             "4. Fund Transfer\n"
-                             "5. Exit\n "
-                             ))
-        if facility >= 1 and facility <= 5:
-            if facility == 1:
-                bobj = Bank(user, account_number[0][0])
-                bobj.balanceequiry()
-            elif facility == 2:
-                while True:
-                    try:
-                        amount = int(input("Enter Amount to Deposit"))
-                        bobj = Bank(user, account_number[0][0])
-                        bobj.deposit(amount)
-                        mydb.commit()
-                        break
-                    except ValueError:
-                        print("Enter Valid Input ie. Number")
-                        continue
+            # Make sure SignIn actually returned a username
+            if user is None:
+                continue
 
-            elif facility == 3:
-                while True:
-                    try:
-                        amount = int(input("Enter Amount to Withdraw"))
-                        bobj = Bank(user, account_number[0][0])
-                        bobj.withdraw(amount)
-                        mydb.commit()
-                        break
-                    except ValueError:
-                        print("Enter Valid Input ie. Number")
-                        continue
-            elif facility == 4:
-                while True:
-                    try:
-                        receive = int(input("Enter Receiver Account Number"))
-                        amount = int(input("Enter Money to Transfer"))
-                        bobj = Bank(user, account_number[0][0])
-                        bobj.fundtransfer(receive, amount)
-                        mydb.commit()
-                        break
-                    except ValueError:
-                        print("Enter Valid Input ie. Number")
-                        continue
-            elif facility == 5:
-                print("Thanks For Using Banking Services")
+            # Extra username validation
+            if any(word in user.lower() for word in blocked):
+                print("Invalid username.")
                 status = False
+                continue
+
+            # Get the account number safely from the database
+            account_number = register.db_query(
+                "SELECT account_number FROM customers WHERE username = %s",
+                (user,)
+            )
+
+            if not account_number:
+                print("Could not find your account.")
+                status = False
+                continue
+
+            status = True
+            break
+
         else:
-            print("Please Enter Valid Input From Options")
-            continue
+            print("Please choose either 1 or 2.")
 
     except ValueError:
-        print("Invalid Input Try Again with Numbers")
-        continue
+        print("Please enter a number.")
+
+# Banking services
+while status:
+
+    print(
+        f"\nWelcome {user.capitalize()}! "
+        "What would you like to do today?\n"
+    )
+
+    try:
+        facility = int(input(
+            "1. Balance Enquiry\n"
+            "2. Cash Deposit\n"
+            "3. Cash Withdraw\n"
+            "4. Fund Transfer\n"
+            "5. Exit\n"
+            "Choose an option: "
+        ))
+
+        if facility == 1:
+
+            bobj = bank.Bank(user, account_number[0][0])
+            bobj.balanceequiry()
+
+        elif facility == 2:
+
+            while True:
+                try:
+                    amount = int(input("Enter amount to deposit: "))
+
+                    if amount <= 0:
+                        print("Please enter an amount greater than 0.")
+                        continue
+
+                    bobj = bank.Bank(user, account_number[0][0])
+                    bobj.deposit(amount)
+
+                    register.mydb.commit()
+
+                    print("Deposit completed successfully.")
+                    break
+
+                except ValueError:
+                    print("Please enter a valid number.")
+
+        elif facility == 3:
+
+            while True:
+                try:
+                    amount = int(input("Enter amount to withdraw: "))
+
+                    if amount <= 0:
+                        print("Please enter an amount greater than 0.")
+                        continue
+
+                    bobj = bank.Bank(user, account_number[0][0])
+                    bobj.withdraw(amount)
+
+                    register.mydb.commit()
+
+                    print("Withdrawal completed successfully.")
+                    break
+
+                except ValueError:
+                    print("Please enter a valid number.")
+
+        elif facility == 4:
+
+            while True:
+                try:
+                    receive = int(
+                        input("Enter receiver account number: ")
+                    )
+
+                    amount = int(
+                        input("Enter amount to transfer: ")
+                    )
+
+                    if receive <= 0 or amount <= 0:
+                        print(
+                            "Account number and amount "
+                            "must be greater than 0."
+                        )
+                        continue
+
+                    bobj = bank.Bank(user, account_number[0][0])
+                    bobj.fundtransfer(receive, amount)
+
+                    register.mydb.commit()
+
+                    print("Transfer completed successfully.")
+                    break
+
+                except ValueError:
+                    print("Please enter valid numbers.")
+
+        elif facility == 5:
+
+            print("Thanks for using Mohit Banking Project!")
+            status = False
+
+        else:
+            print("Please choose an option between 1 and 5.")
+
+    except ValueError:
+        print("Please enter a valid number.")
+```
